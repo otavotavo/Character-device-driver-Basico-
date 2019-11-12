ed0e8385f625a9730ad7ddb08cea64ed10b5ebb8
otavotavo <octaviogodoy123@gmail.com>
2019-11-12 17:00:20 -0300

    Create README.mk

---
+++ README.mk
@@ -0,0 +1,4 @@
+Um character device driver para linux que indica quantas vezes foi lido.
+
+Exemplo para trabalhar noçoes de FILE,fops, funcionamento básico do kernel linux, kernel data e user data segment,licensas e 
+programaçao de módulos.
